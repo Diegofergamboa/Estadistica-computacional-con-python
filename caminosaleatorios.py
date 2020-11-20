46ac812b8174ffe85720b534209601d31708ee84
Diego Ortiz Gamboa <diegofergamboa@hotmail.es>
2020-11-19 22:28:52 -0500

    Se han agregado contenidos vistos

---
+++ caminosaleatorios.py
@@ -0,0 +1,11 @@
+'''
+    Deterministas = Que al mismo input obtenemos el mismo output
+    Ciertos problemas que por su proopia naturaleza tienen aleatoriedad es decir que son random
+    Debemos saber como colocar esto en nuestros propios programas
+'''
+#   Elegir decisiones dentro de un set de decisiones validas de manera aleatoria = Aleatoriedad
+#   EL resultado del programa va a ser distinto
+#   Se utiliza en el movimiento Brawniano, asi mismo en las simulaciones de particulas como es el caso del humo
+#   Permite ademas generar simulaciones en la bolsas de valor
+#   Escultura de nube cuantica
+
